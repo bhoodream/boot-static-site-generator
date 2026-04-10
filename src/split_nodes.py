c396225329a5e2ba57fa5eb217159d5621061df1
Vadim Fedorov <bhoodream@yandex.ru>
2026-04-10 23:18:28 +0700

    split nodes images/links

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,19 +1,81 @@
 from textnode import TextNode, TextType
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+
+def _split_nodes_generic(old_nodes, extract_func, text_type, format_func):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        extracted_items = extract_func(node.text)
+        if not extracted_items:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = node.text
+        for item in extracted_items:
+            label, url = item
+            actual_url = url if url != "" else None
+
+            markdown = format_func(label, url)
+            sections = remaining_text.split(markdown, 1)
+
+            if len(sections) != 2:
+                continue
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(label, text_type, actual_url))
+            remaining_text = sections[1]
+
+        if remaining_text != "":
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+    return new_nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        if node.text_type == TextType.TEXT:
-            if delimiter in node.text:
-                parts = node.text.split(delimiter)
-                if len(parts) < 3:
-                    raise ValueError(f"Invalid delimiter: {delimiter}")
-                new_nodes.append(TextNode(parts[0], TextType.TEXT))
-                new_nodes.append(TextNode(parts[1], text_type))
-                new_nodes.append(TextNode(parts[2], TextType.TEXT))
-            else:
-                new_nodes.append(node)
-        else:
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
+            continue
+
+        if delimiter not in node.text:
+            new_nodes.append(node)
+            continue
+
+        parts = node.text.split(delimiter)
+        if len(parts) % 2 == 0:
+            raise ValueError(f"Invalid delimiter: {delimiter} in {node.text}")
+
+        for i in range(len(parts)):
+            if parts[i] == "" and i % 2 == 0:
+                continue
+
+            if i % 2 == 0:
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(parts[i], text_type))
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    return _split_nodes_generic(
+        old_nodes,
+        extract_markdown_images,
+        TextType.IMAGE,
+        lambda alt, url: f"![{alt}]({url})",
+    )
+
+
+def split_nodes_link(old_nodes):
+    return _split_nodes_generic(
+        old_nodes,
+        extract_markdown_links,
+        TextType.LINK,
+        lambda text, url: f"[{text}]({url})",
+    )
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -1,6 +1,10 @@
 import unittest
 
-from split_nodes import split_nodes_delimiter
+from split_nodes import (
+    split_nodes_delimiter,
+    split_nodes_image,
+    split_nodes_link,
+)
 from textnode import TextNode, TextType
 
 
@@ -56,3 +60,88 @@ class TestSplitNodes(unittest.TestCase):
         node = TextNode("This has an opening ` but no closing", TextType.TEXT)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_split_nodes_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zcew34n.png) and ![another](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zcew34n.png"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+        )
+
+    def test_split_nodes_image_single(self):
+        node = TextNode("![image](https://www.example.com/image.png)", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("image", TextType.IMAGE, "https://www.example.com/image.png"),
+            ],
+        )
+
+    def test_split_nodes_image_no_images(self):
+        node = TextNode("This text has no images.", TextType.TEXT)
+        new_nodes = split_nodes_image([node])
+        self.assertEqual(new_nodes, [node])
+
+    def test_split_nodes_link(self):
+        node = TextNode(
+            "This is text with a [link](https://www.google.com) and [another](https://www.example.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another", TextType.LINK, "https://www.example.com"),
+            ],
+        )
+
+    def test_split_nodes_link_single(self):
+        node = TextNode("[link](https://www.google.com)", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+            ],
+        )
+
+    def test_split_nodes_link_no_links(self):
+        node = TextNode("This text has no links.", TextType.TEXT)
+        new_nodes = split_nodes_link([node])
+        self.assertEqual(new_nodes, [node])
+
+    def test_split_nodes_mixed(self):
+        node = TextNode(
+            "Text with [link](https://boot.dev) and ![image](https://i.imgur.com/zcew34n.png)",
+            TextType.TEXT,
+        )
+        # First split links
+        nodes = split_nodes_link([node])
+        # Then split images
+        nodes = split_nodes_image(nodes)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("Text with ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zcew34n.png"),
+            ],
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
