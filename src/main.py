8e31429259bd4150442da8712014dbba0e9b90e9
Vadim Fedorov <bhoodream@yandex.ru>
2026-04-11 14:33:24 +0700

    content and gen recursive

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import shutil
 import subprocess
 
 from copystatic import copy_files_recursive
-from generate_page import generate_page
+from generate_pages_recursive import generate_pages_recursive
 
 
 dir_path_static = "./static"
@@ -20,12 +20,8 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating page...")
-    generate_page(
-        os.path.join(dir_path_content, "index.md"),
-        template_path,
-        os.path.join(dir_path_public, "index.html"),
-    )
+    print("Generating pages...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
     print("Starting server on port 8888...")
     subprocess.run(["python3", "-m", "http.server", "8888"], cwd=dir_path_public)
